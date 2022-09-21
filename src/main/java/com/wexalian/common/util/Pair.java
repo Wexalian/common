0383be823749641592cb9b90c988515e5c3e9c4e
Wexalian <wexalian@gmail.com>
2022-09-21 18:04:15 +0200

    Add simple Pair utility class

---
+++ src/main/java/com/wexalian/common/util/Pair.java
@@ -0,0 +1,27 @@
+package com.wexalian.common.util;
+
+import com.wexalian.nullability.annotations.Nullable;
+
+public final class Pair<L, R> {
+    private final L left;
+    private final R right;
+    
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+    
+    @Nullable
+    public L getLeft() {
+        return left;
+    }
+    
+    @Nullable
+    public R getRight() {
+        return right;
+    }
+    
+    public static <L, R> Pair<L, R> of(@Nullable L left, @Nullable R right) {
+        return new Pair<>(left, right);
+    }
+}
