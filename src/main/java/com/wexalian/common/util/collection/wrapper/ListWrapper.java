c6a34a21f0fe80a6019a95c51007a1ab9d231c87
Wexalian <wexalian@gmail.com>
2022-09-26 21:59:23 +0200

    Add simple List and Map wrappers

---
+++ src/main/java/com/wexalian/common/util/collection/wrapper/ListWrapper.java
@@ -0,0 +1,138 @@
+package com.wexalian.common.util.collection.wrapper;
+
+import com.wexalian.nullability.annotations.Nonnull;
+import com.wexalian.nullability.annotations.Nullable;
+
+import java.util.*;
+
+@FunctionalInterface
+public interface ListWrapper<T> extends List<T> {
+    
+    @Nonnull
+    List<T> get();
+    
+    @Override
+    default int size() {
+        return get().size();
+    }
+    
+    @Override
+    default boolean isEmpty() {
+        return get().isEmpty();
+    }
+    
+    @Override
+    default boolean contains(@Nonnull Object object) {
+        return get().contains(object);
+    }
+    
+    @Override
+    @Nonnull
+    default Iterator<T> iterator() {
+        return get().iterator();
+    }
+    
+    @Override
+    @Nonnull
+    default Object[] toArray() {
+        return get().toArray();
+    }
+    
+    @Override
+    @Nonnull
+    @SuppressWarnings("SuspiciousToArrayCall")
+    default <T1> T1[] toArray(T1[] a) {
+        return get().toArray(a);
+    }
+    
+    @Override
+    default boolean add(@Nonnull T t) {
+        return get().add(t);
+    }
+    
+    @Override
+    default boolean remove(@Nonnull Object object) {
+        return get().remove(object);
+    }
+    
+    @Override
+    default boolean containsAll(@Nonnull Collection<?> collection) {
+        return new HashSet<>(get()).containsAll(collection);
+    }
+    
+    @Override
+    default boolean addAll(@Nonnull Collection<? extends T> collection) {
+        return get().addAll(collection);
+    }
+    
+    @Override
+    default boolean addAll(int index, @Nonnull Collection<? extends T> collection) {
+        return get().addAll(index, collection);
+    }
+    
+    @Override
+    default boolean removeAll(@Nonnull Collection<?> collection) {
+        return get().removeAll(collection);
+    }
+    
+    @Override
+    default boolean retainAll(@Nonnull Collection<?> collection) {
+        return get().retainAll(collection);
+    }
+    
+    @Override
+    default void clear() {
+        get().clear();
+    }
+    
+    @Override
+    @Nullable
+    default T get(int index) {
+        return get().get(index);
+    }
+    
+    @Override
+    @Nullable
+    default T set(int index, @Nonnull T element) {
+        return get().set(index, element);
+    }
+    
+    @Override
+    default void add(int index, @Nonnull T element) {
+        get().add(index, element);
+    }
+    
+    @Override
+    @Nullable
+    default T remove(int index) {
+        return get().remove(index);
+    }
+    
+    @Override
+    default int indexOf(@Nonnull Object object) {
+        return get().indexOf(object);
+    }
+    
+    @Override
+    default int lastIndexOf(@Nonnull Object object) {
+        return get().lastIndexOf(object);
+    }
+    
+    @Override
+    @Nonnull
+    default ListIterator<T> listIterator() {
+        return get().listIterator();
+    }
+    
+    @Override
+    @Nonnull
+    default ListIterator<T> listIterator(int index) {
+        return get().listIterator(index);
+    }
+    
+    @Override
+    @Nonnull
+    default List<T> subList(int fromIndex, int toIndex) {
+        return get().subList(fromIndex, toIndex);
+    }
+}
