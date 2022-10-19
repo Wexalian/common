ae79a93db6a1f484a32fe5817b26574f58d6dc7c
Wexalian <larsgerrits+wexalian@github.com>
2022-10-20 00:05:47 +0200

    Move export of collection utils in module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,12 @@ module com.wexalian.common {
     
     requires com.google.gson;
     
+    exports com.wexalian.common.collection;
+    exports com.wexalian.common.collection.wrapper;
     exports com.wexalian.common.gson;
     exports com.wexalian.common.stream;
     exports com.wexalian.common.unchecked;
     exports com.wexalian.common.util;
-    exports com.wexalian.common.collection;
-    exports com.wexalian.common.collection.wrapper;
     
     opens com.wexalian.common.gson to com.google.gson;
 }
