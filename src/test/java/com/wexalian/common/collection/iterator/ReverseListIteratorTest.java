cc51b4c71fa0685ae0116b9717c7e58ddc8d159e
Wexalian <larsgerrits+wexalian@github.com>
2022-11-21 23:06:30 +0100

    Add ReverseListIteratorTest

---
+++ src/test/java/com/wexalian/common/collection/iterator/ReverseListIteratorTest.java
@@ -0,0 +1,20 @@
+package com.wexalian.common.collection.iterator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ReverseListIteratorTest {
+    
+    private static final List<String> TEST_LIST = List.of("One", "Two", "Three", "Four", "Five");
+    
+    @Test
+    public void testReverseListIterator() {
+        for (String value : TEST_LIST) {
+            System.out.println(value);
+        }
+        for (String value : ReverseListIterator.of(TEST_LIST)) {
+            System.out.println(value);
+        }
+    }
+}
