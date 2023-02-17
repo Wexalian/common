cf990e0e9b704b1d695f8a05d6309e3dfe053509
Wexalian <larsgerrits+wexalian@github.com>
2023-02-17 23:21:01 +0100

    Go back to single project structure and clean up files

--- common-plugin-test/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-import com.wexalian.common.plugin.IAbstractPlugin;
-import com.wexalian.common.plugin.test_impl.TestPlugin;
-
-module com.wexalian.plugin.test {
-    requires com.wexalian.nullability;
-    requires com.wexalian.common;
-    
-    provides IAbstractPlugin with TestPlugin;
-}
