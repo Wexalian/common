21c9a6df362c0126e570487bf3232d5a14b1529f
Wexalian <larsgerrits+wexalian@github.com>
2023-03-19 00:27:12 +0100

    Filter contents of `FilesUtil#read` and `FilesUtil#write` to non-null

--- src/main/java/com/wexalian/common/util/FilesUtil.java
+++ src/main/java/com/wexalian/common/util/FilesUtil.java
@@ -5,18 +5,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class FilesUtil {
     public static <T> List<T> read(Path path, Function<String, T> mapping) throws IOException {
-        try (Stream<String> lines = Files.lines(path)) {
-            return lines.map(mapping).collect(Collectors.toList());
+        if (Files.exists(path)) {
+            try (Stream<String> lines = Files.lines(path)) {
+                return lines.filter(StringUtil::isNotBlank).map(mapping).collect(Collectors.toList());
+            }
         }
+        return List.of();
     }
     
     public static <T> Path write(Path path, Collection<T> contents, Function<T, String> mapping) throws IOException {
-        return Files.write(path, contents.stream().map(mapping).toList());
+        return Files.write(path, contents.stream().filter(Objects::nonNull).map(mapping).toList());
     }
 }
