c224e8ba5629822706d61a3b590960bc075693d5
Wexalian <larsgerrits+wexalian@github.com>
2023-03-19 00:27:47 +0100

    Added nullability annotations

--- src/main/java/com/wexalian/common/util/FilesUtil.java
+++ src/main/java/com/wexalian/common/util/FilesUtil.java
@@ -1,5 +1,7 @@
 package com.wexalian.common.util;
 
+import com.wexalian.nullability.annotations.Nonnull;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +13,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class FilesUtil {
-    public static <T> List<T> read(Path path, Function<String, T> mapping) throws IOException {
+    @Nonnull
+    public static <T> List<T> read(@Nonnull Path path, @Nonnull Function<String, T> mapping) throws IOException {
         if (Files.exists(path)) {
             try (Stream<String> lines = Files.lines(path)) {
                 return lines.filter(StringUtil::isNotBlank).map(mapping).collect(Collectors.toList());
@@ -20,7 +23,8 @@ public final class FilesUtil {
         return List.of();
     }
     
-    public static <T> Path write(Path path, Collection<T> contents, Function<T, String> mapping) throws IOException {
+    @Nonnull
+    public static <T> Path write(@Nonnull Path path, @Nonnull Collection<T> contents, @Nonnull Function<T, String> mapping) throws IOException {
         return Files.write(path, contents.stream().filter(Objects::nonNull).map(mapping).toList());
     }
 }
