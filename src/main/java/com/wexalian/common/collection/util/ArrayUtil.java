d8225f01801944d81333727ca18a055be270ea9d
Wexalian <larsgerrits+wexalian@github.com>
2023-03-25 07:59:46 +0100

    Fix `ArrayUtil#isEmpty`

--- src/main/java/com/wexalian/common/collection/util/ArrayUtil.java
+++ src/main/java/com/wexalian/common/collection/util/ArrayUtil.java
@@ -5,10 +5,9 @@ import com.wexalian.nullability.annotations.Nullable;
 public final class ArrayUtil {
     
     public static <T> boolean isEmpty(@Nullable T[] array) {
-        if (array == null || array.length == 0) return false;
-        
+        if (array == null) return true;
         for (T t : array) {
-            if (t == null) return false;
+            if (t != null) return false;
         }
         return true;
     }
