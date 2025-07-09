1111b50b1963e76c4399afa21c08bd976f2c25b6
Wexalian <wexalian@gmail.com>
2025-07-09 21:51:34 +0200

    Condense all 6 string isNull and isBlank methods into 2 single methods

--- src/main/java/com/wexalian/common/util/StringUtil.java
+++ src/main/java/com/wexalian/common/util/StringUtil.java
@@ -13,31 +13,15 @@ import java.util.stream.Collectors;
 
 public class StringUtil {
     private static final Pattern FORMATTING_PATTERN = Pattern.compile("\\{([^}]*)}");
-    
-    public static boolean isNull(@Nullable String string) {
-        return string == null;
+
+    public static boolean isBlank(@Nullable String string) {
+        return string == null || string.isBlank();
     }
-    
-    public static boolean isNotNull(@Nullable String string) {
-        return !isNull(string);
-    }
-    
-    public static boolean isBlank(@Nonnull String string) {
-        return string.isBlank();
-    }
-    
-    public static boolean isNotBlank(@Nonnull String string) {
+
+    public static boolean isNotBlank(@Nullable String string) {
         return !isBlank(string);
     }
-    
-    public static boolean isNullOrBlank(@Nullable String string) {
-        return isNull(string) || isBlank(string);
-    }
-    
-    public static boolean isNotNullOrBlank(@Nullable String string) {
-        return !isNullOrBlank(string);
-    }
-    
+
     @Nonnull
     public static String format(@Nonnull String text, @Nonnull Object[] params) {
         String formatted = text;
@@ -46,45 +30,46 @@ public class StringUtil {
         }
         return formatted;
     }
-    
+
     @Nonnull
     public static String format(@Nonnull String text, @Nonnull Object first, @Nonnull Object... params) {
         return format(formatParam(text, first), params);
     }
-    
+
     private static String formatParam(String text, Object param) {
         Matcher matcher = FORMATTING_PATTERN.matcher(text);
         if (matcher.find()) {
-            return matcher.replaceFirst(r -> !r.group(1).isBlank() ? String.format(r.group(1), param) : Matcher.quoteReplacement(param.toString()));
+            return matcher.replaceFirst(r -> !r.group(1)
+                                               .isBlank() ? String.format(r.group(1), param) : Matcher.quoteReplacement(param.toString()));
         }
         return text;
     }
-    
+
     @Nonnull
     public static <T> String join(@Nullable T[] items, @Nonnull String delimiter) {
         return join(items, delimiter, Object::toString);
     }
-    
+
     @Nonnull
     public static <T> String join(@Nullable Collection<T> items, @Nonnull String delimiter) {
         return join(items, delimiter, Object::toString);
     }
-    
+
     @Nonnull
     public static <T> String join(@Nullable T[] items, @Nonnull String delimiter, @Nonnull Function<T, String> formatter) {
         return join(ListUtil.nonnull(items), delimiter, formatter);
     }
-    
+
     @Nonnull
     public static <T> String join(@Nullable Collection<T> items, @Nonnull String delimiter, @Nonnull Function<T, String> formatter) {
         if (items == null || items.isEmpty()) return "";
         return items.stream().map(formatter).collect(Collectors.joining(delimiter));
     }
-    
+
     @Nonnull
     public static String slug(@Nonnull String text) {
-        return Normalizer.normalize(text, Normalizer.Form.NFD)
-                         .replaceAll("[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+", "")
+        return Normalizer.normalize(text, Normalizer.Form.NFKD)
+                         .replaceAll("\\p{M}", "")
                          .toLowerCase()
                          .replace(' ', '_')
                          .replace('-', '_')
