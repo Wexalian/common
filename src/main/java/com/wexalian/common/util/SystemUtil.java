60472134a15757bf913ee6e7954a3746de1863d5
Wexalian <wexalian@gmail.com>
2025-07-09 21:50:26 +0200

    Update SystemUtil IS_MAC check

--- src/main/java/com/wexalian/common/util/SystemUtil.java
+++ src/main/java/com/wexalian/common/util/SystemUtil.java
@@ -4,7 +4,7 @@ public class SystemUtil {
     public static final String OPERATING_SYSTEM = System.getProperty("os.name");
     
     public static final boolean IS_WINDOWS = doesOsStartWith("Windows");
-    public static final boolean IS_MAC_OSX = doesOsStartWith("Mac OS X");
+    public static final boolean IS_MAC = doesOsStartWith("Mac");
     
     public static boolean doesOsStartWith(String prefix) {
         return OPERATING_SYSTEM.startsWith(prefix);
